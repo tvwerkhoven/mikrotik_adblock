fa3374002988aca81aa25dc51a244064f3d20d1b
Tim van Werkhoven <tvwerkhoven@github>
2024-03-10 11:13:51 +0100

    Adding dated output file & deploy commands

--- mkvyosadblock.sh
+++ mkvyosadblock.sh
@@ -23,10 +23,13 @@ WGET_INSTALLED=false
 command -v curl >/dev/null 2>&1 && CURL_INSTALLED=true
 command -v wget >/dev/null 2>&1 && WGET_INSTALLED=true
 
+OUTFILE=$(date "+%Y%m%d")_adblock.all.vyos.txt
+
 # Select which lists to use
 # USELISTS=(list.disconnect.*.disc.txt list.adaway.hosts.txt list.adguard.hosts.txt list.yoyo.hosts.txt list.easylistdutch.tpl.txt)
 # USELISTS=(list.yoyo.hosts.txt list.easylistdutch.tpl.txt)
-USELISTS=(list.adaway.hosts.txt list.adguardmobilespyware.hosts.txt list.adguardmobileads.hosts.txt list.yoyo.hosts.txt list.easylistdutch.tpl.txt)
+USELISTS=(list.adaway.hosts.txt list.adguardmobilespyware.hosts.txt list.adguardmobileads.hosts.txt list.easylistdutch.tpl.txt list.yoyo.hosts.txt list.oisd.dnsmasq.txt)
+USELISTS=(list.adguardmobilespyware.hosts.txt list.adguardmobileads.hosts.txt  list.oisd.dnsmasq.txt)
 
 # Collect source lists
 collect_source_list() {
@@ -49,17 +52,18 @@ collect_source_list "https://raw.githubusercontent.com/r-a-y/mobile-hosts/master
 collect_source_list "https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardMobileSpyware.txt" "list.adguardmobilespyware.hosts.txt"
 collect_source_list "https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardMobileAds.txt" "list.adguardmobileads.hosts.txt"
 collect_source_list "https://easylist-msie.adblockplus.org/easylistdutch.tpl" "list.easylistdutch.tpl.txt"
-collect_source_list "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext"  "list.yoyo.hosts.txt"collect_source_list "https://dnsmasq.oisd.nl/basic/" "oisd.dnsmasq.txt"
+collect_source_list "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext"  "list.yoyo.hosts.txt"
+collect_source_list "https://small.oisd.nl/dnsmasq" "list.oisd.dnsmasq.txt"
 
 
 ### Process for VyOS / PowerDNS
 # See https://gist.github.com/ahupowerdns/bb1a043ce453a9f9eeed and https://www.vanwerkhoven.org/blog/2023/dns-based-adblocking-on-vyos/
 
 rm -f /tmp/adblock.all.vyos.txt
-rm -f adblock.all.vyos.txt
+rm -f ${OUTFILE}
 
 # Format should be like return{"101com.com", "101order.com"}
-echo -n "return{\"101com.com\"" >> adblock.all.vyos.txt
+echo -n "return{\"101com.com\"" >> ${OUTFILE}
 
 # Process disconnect-format lists
 for f in list.*.disc.txt; do
@@ -71,6 +75,11 @@ for f in list.*.hosts.txt; do
 	[[ ${USELISTS[*]} =~ "$f" ]] && grep -v \^\# $f | grep -v "localhost" | awk '{print ", \""$2"\""}' >> /tmp/adblock.all.vyos.txt
 done
 
+# Process hosts-format lists
+for f in list.*.dnsmasq.txt; do
+	[[ ${USELISTS[*]} =~ "$f" ]] && grep -v \^\# $f | awk -F  "/" '{print ", \""$2"\""}' >> /tmp/adblock.all.vyos.txt
+done
+
 # Process TPL-format lists
 # https://docs.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/platform-apis/hh273399(v=vs.85)?redirectedfrom=MSDN#creatingtpls
 # https://www.malwaredomainlist.com/forums/index.php?topic=4517.0
@@ -79,7 +88,11 @@ for f in list.*.tpl.txt; do
 done
 
 # Filter out duplicates, count, skip first line because it's an empty host (usually)
-sort /tmp/adblock.all.vyos.txt | uniq | tail -n +2 | tr -d '\n' >> adblock.all.vyos.txt
-wc -w adblock.all.vyos.txt
+sort /tmp/adblock.all.vyos.txt | uniq | tail -n +2 | tr -d '\n' >> ${OUTFILE}
+wc -w ${OUTFILE}
 
-echo "}" >> adblock.all.vyos.txt
+echo "}" >> ${OUTFILE}
+
+echo "to deploy:"
+echo "scp ${OUTFILE} vyos:/config/scripts/pdns-adblock-blocklist.lua"
+echo "vyos: restart dns forwarding"
